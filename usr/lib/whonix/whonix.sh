6f46725d13f226fc786d0e8389d81530bbf18da6
Patrick Schleizer <adrelanos@riseup.net>
2015-01-13 11:53:10 +0000

    set: export TOR_HIDE_BROWSER_LOGO=1 Hide the Tor Browser Bundle (TBB) logo in tor-launcher. This is useful to avoid users confusing TBB and Whonix. Also useful when running tor-launcher in standalone mode, because then it's not TBB that is starting. Lastly also useful avoid ​trademark issues when redistributing original, unmodified TBB in (linux) distributions. https://trac.torproject.org/projects/tor/ticket/14122 https://www.torproject.org/docs/trademark-faq.html.en https://www.whonix.org/wiki/Dev/TPO_Trademark

--- usr/lib/whonix/whonix.sh
+++ usr/lib/whonix/whonix.sh
@@ -7,3 +7,13 @@
 ## TorBirdy reads this variable and sets appropriate socks proxy settings for
 ## Stream Isolation.
 export WHONIX=1
+
+## Hide the Tor Browser Bundle (TBB) logo in tor-launcher. This is useful to
+## avoid users confusing TBB and Whonix. Also useful when running tor-launcher
+## in standalone mode, because then it's not TBB that is starting. Lastly also
+## useful avoid ​trademark issues when redistributing original, unmodified TBB
+## in (linux) distributions.
+## https://trac.torproject.org/projects/tor/ticket/14122
+## https://www.torproject.org/docs/trademark-faq.html.en
+## https://www.whonix.org/wiki/Dev/TPO_Trademark
+export TOR_HIDE_BROWSER_LOGO=1
