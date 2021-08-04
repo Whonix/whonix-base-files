55d7fbd3ed6785666b158e8b076ec542fb0fa484
Patrick Schleizer <adrelanos@whonix.org>
2021-08-04 07:56:23 -0400

    lintian FHS

--- etc/profile.d/20_whonix.sh
+++ etc/profile.d/20_whonix.sh
@@ -3,6 +3,6 @@
 ## Copyright (C) 2012 - 2021 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-if [ -x /usr/lib/whonix/whonix.sh ]; then
-   . /usr/lib/whonix/whonix.sh
+if [ -x /usr/libexec/whonix-base-files/whonix.sh ]; then
+   . /usr/libexec/whonix-base-files/whonix.sh
 fi
